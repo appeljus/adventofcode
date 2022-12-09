149c753b0d8d3225822ee3f47af16826d7ae28c1
Martijn de Bijl <martijn.de.bijl@knmi.nl>
2022-12-09 13:17:13 +0100

    Part 2 of puzzle 9.

--- 2022/puzzle9.py
+++ 2022/puzzle9.py
@@ -1,7 +1,7 @@
 # %%
 
 def touching(head_pos: tuple[int, int], tail_pos: tuple[int, int]) -> bool:
-    if abs(head_pos[0]-tail_pos[0]) < 2 and abs(head_pos[1]-tail_pos[1]) < 2:
+    if abs(head_pos[0] - tail_pos[0]) < 2 and abs(head_pos[1] - tail_pos[1]) < 2:
         return True
     else:
         return False
@@ -9,13 +9,13 @@ def touching(head_pos: tuple[int, int], tail_pos: tuple[int, int]) -> bool:
 
 def move_head(head_pos: tuple[int, int], command: str) -> tuple[int, int]:
     if command == "R":
-        return head_pos[0] + 1, head_pos[1]
+        return (head_pos[0] + 1), head_pos[1]
     elif command == "L":
-        return head_pos[0] - 1, head_pos[1]
+        return (head_pos[0] - 1), head_pos[1]
     elif command == "U":
-        return head_pos[0], head_pos[1] + 1
+        return head_pos[0], (head_pos[1] + 1)
     elif command == "D":
-        return head_pos[0], head_pos[1] - 1
+        return head_pos[0], (head_pos[1] - 1)
     else:
         print("UNKNOWN COMMAND")
 
@@ -54,20 +54,38 @@ def move_tail(head_pos: tuple[int, int], tail_pos: tuple[int, int]) -> tuple[int
     return result
 
 
+# %%
 head_position = 0, 0
 tail_position = 0, 0
 tail_visited = [tail_position]
 with open('./2022/input_puzzle_9.txt', 'r') as fp:
     for line in fp:
         move_command = line[0]
-        num_steps = line[2]
-        for i in range(int(num_steps)):
+        num_steps = int(line[2:])
+        for i in range(num_steps):
             head_position = move_head(head_position, move_command)
-            print(f"head position:\t{head_position}")
             if not touching(head_position, tail_position):
                 tail_position = move_tail(head_position, tail_position)
-                print(f"tail position:\t{tail_position}")
                 tail_visited.append(tail_position)
 
+print(len(set(tail_visited)))
 
+# %%
+
+num_knots = 10
+knot_positions = [(0, 0)] * num_knots
+tail_visited = [knot_positions[-1]]
+with open('./2022/input_puzzle_9.txt', 'r') as fp:
+    for line in fp:
+        move_command = line[0]
+        num_steps = int(line[2:])
+        for _ in range(num_steps):
+            knot_positions[0] = move_head(knot_positions[0], move_command)
+            for i in range(1, num_knots):
+                previous_knot_position = knot_positions[i - 1]
+                if not touching(previous_knot_position, knot_positions[i]):
+                    knot_positions[i] = move_tail(previous_knot_position, knot_positions[i])
+                    if i == (num_knots - 1):
+                        print(f"tail position: {knot_positions[i]}")
+                        tail_visited.append(knot_positions[i])
 print(len(set(tail_visited)))
